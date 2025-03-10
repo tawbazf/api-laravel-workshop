f3fd97d1c80721e5be1a4bbb522c5655b796a35f
tawbazf <tawbazehaf@gmail.com>
2025-03-10 10:42:26 +0100

    migration fonction up

--- database/migrations/2025_03_10_094113_create_products_table.php.php
+++ database/migrations/2025_03_10_094113_create_products_table.php.php
@@ -9,11 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        //
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+        });
     }
-
     /**
      * Reverse the migrations.
      */

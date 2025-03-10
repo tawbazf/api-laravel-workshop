aaf53b8026ca9316f7b46274dd20858f3c76beb1
tawbazf <tawbazehaf@gmail.com>
2025-03-10 12:31:26 +0100

    last commit

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Route::apiResource('products', ProductController::class);
-// Route::post('/products', [ProductController::class, 'store']);
+
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
